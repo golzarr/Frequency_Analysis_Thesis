5290a294565bf0e77717ca87f4d09ed06db1db92
unknown <andrej.zajac.dev@gmail.com>
2023-02-14 17:09:43 +0100

    Draft 7.0 Python Files

--- target/classes/frequencyAnalysis.py
+++ target/classes/frequencyAnalysis.py
@@ -1,99 +1,38 @@
-class AndrejThesis() :
-    @staticmethod  
-    def main() :
-        # 		 * Tries all 25 possible shifts to produce a list of likely inputs to a
-        # 		 * caesar-cipher encoded message. Displays the decryption possibility only when
-        # 		 * the frequency of any vowel is above 5%.
-        source = "Pm ol ohk hufaopun jvumpkluaphs av zhf, ol dyval pa pu jpwoly, aoha pz, if zv johunpun aol vykly vm aol slaalyz vm aol hswohila, aoha uva h dvyk jvbsk il thkl vba.".upper()
-        # Checking if the user wants to exit
-      
-        print("Bye.")
-            
-        sourceText = [' '] * (len(source))
-        unicode = [0] * (len(source))
-        unicodeCopy = [0] * (len(source))
-        # Moves source text into array
-        count = 0
-        while (count < len(source)) :
-            sourceText[count] = source[count]
-            count += 1
-        hex = None
-        dec = 0
-        # Go through sourceText and translate each letter into unicode value.
-        count = 0
-        while (count < len(sourceText)) :
-            # Convert to hex and then to decimal representation of char
-            hex = str(hex(sourceText[count]))
-            dec = Integer.parseInt(hex,16)
-            unicode[count] = dec
-            unicodeCopy[count] = dec
-            count += 1
-        shift = 1
-        while (shift <= 25) :
-            BulkUpload.AndrejThesis.smartShift(shift, unicode, unicodeCopy)
-            shift += 1
-    @staticmethod
-    def smartShift( shift,  unicode,  unicodeCopy) :
-        # Preserve values
-        x = 0
-        while (x <= len(unicode) - 1) :
-            unicodeCopy[x] = unicode[x]
-            # Shift and recovery
-            if (unicode[x] >= 65 and unicode[x] <= 90) :
-                unicodeCopy[x] += shift
-                if (unicodeCopy[x] > 90) :
-                    unicodeCopy[x] -= 26
-            x += 1
-        processed = [None] * (len(unicode))
-        finalProcess = [' '] * (len(unicode))
-        # Converts dec to hex string
-        count = 0
-        while (count < len(processed)) :
-            processed[count] = str(hex(unicodeCopy[count]))
-            # Convert the hex string into char:
-            hexToInt = Integer.parseInt(processed[count],16)
-            intToChar = chr(hexToInt)
-            finalProcess[count] = intToChar
-            count += 1
-        # Basic frequency calculations
-        total = 0
-        aTotal = 0
-        eTotal = 0
-        iTotal = 0
-        oTotal = 0
-        uTotal = 0
-        # Analyze array to count frequency of vowels
-        for c in finalProcess :
-            total += 1
-            if (c=='A'):
-                aTotal += 1
-            elif(c=='E'):
-                eTotal += 1
-            elif(c=='I'):
-                iTotal += 1
-            elif(c=='O'):
-                oTotal += 1
-            elif(c=='U'):
-                uTotal += 1
-          
-        # Frequency calculations
-        finalCrypto = ""
-        for c in finalProcess :
-            finalCrypto += c
-        if (eTotal / total >= 0.05 or aTotal / total >= 0.05 or iTotal / total >= 0.05 or oTotal / total >= 0.05 or uTotal / total >= 0.05) :
-            print()
-            print("\t" + finalCrypto)
-            print("\t\tA Pct: " + str(aTotal / total))
-            print("\t\tE Pct: " + str(eTotal / total))
-            print("\t\tI Pct: " + str(iTotal / total))
-            print("\t\tO Pct: " + str(oTotal / total))
-            print("\t\tU Pct: " + str(uTotal / total))
-        else :
-            print()
-            print("NOT Potential: " + finalCrypto)
-    
+import matplotlib.pylab as plt
 
-if __name__=="__main__":
-    print("File one executed when ran directly")
-    AndrejThesis.main()
-    print("File one executed when ran directly 2") 
+# these are the letters we are interested in when dealing with frequency-analysis
+# WHITE SPACE IS THE MOST FREQUENT 'LETTER' IN THE ENGLISH ALPHABET !!!
+LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+
+# the method to do frequency analysis: we just count the occurrences
+# of the given characters
+def frequency_analysis(text):
+    # the text we analyse
+    text = text.upper()
+
+    # we use a dictionary to store the letter-frequency pair
+    letter_frequencies = {}
+
+    # initialize the dictionary (of course with 0 frequencies)
+    for letter in LETTERS:
+        letter_frequencies[letter] = 0
+
+    # let's consider the text we want to analyse
+    for letter in text:
+        if letter in LETTERS:
+            letter_frequencies[letter] += 1
+
+    return letter_frequencies
+
+
+def plot_distribution(frequencies):
+    plt.bar(frequencies.keys(), frequencies.values())
+    plt.show()
+
+
+if __name__ == '__main__':
+
+    plain_text = "ZHOFRPHCWRCWKHCDQGUHMCIUHTXHQFACDQDOAVLVCSURMHFW"
+    freq = frequency_analysis(plain_text)
+    plot_distribution(freq)
