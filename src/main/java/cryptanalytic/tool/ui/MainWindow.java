5b3585adc988f5a2a553178bc70a1679c05d9280
unknown <andrej.zajac.dev@gmail.com>
2023-02-14 06:27:29 +0100

    Better UI and Phython

--- src/main/java/cryptanalytic/tool/ui/MainWindow.java
+++ src/main/java/cryptanalytic/tool/ui/MainWindow.java
@@ -10,10 +10,12 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 public class MainWindow {
 
 	private JFrame frmFrequencyAnalysisV;
+	private JTextField txtAlphabet;
 
 	/**
 	 * Launch the application.
@@ -44,41 +46,85 @@ public class MainWindow {
 	private void initialize() {
 		frmFrequencyAnalysisV = new JFrame();
 		frmFrequencyAnalysisV.setTitle("Frequency Analysis v1.0");
-		frmFrequencyAnalysisV.setBounds(100, 100, 736, 562);
+		frmFrequencyAnalysisV.setBounds(100, 100, 736, 581);
 		frmFrequencyAnalysisV.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmFrequencyAnalysisV.getContentPane().setLayout(new GridLayout(1, 0, 0, 0));
 
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		frmFrequencyAnalysisV.getContentPane().add(tabbedPane);
 
+	
+
 		JPanel panel = new JPanel();
-		tabbedPane.addTab("main", null, panel, null);
+		tabbedPane.addTab("Caesar-Cipher", null, panel, null);
 		panel.setLayout(null);
 
-		JLabel lblNewLabel = new JLabel("Input");
-		lblNewLabel.setBounds(44, 35, 56, 16);
-		panel.add(lblNewLabel);
-
-		JLabel lblNewLabel_1 = new JLabel("Cipher Text");
-		lblNewLabel_1.setBounds(44, 75, 110, 16);
+		JLabel lblNewLabel_1 = new JLabel("plain_text / cipher_text");
+		lblNewLabel_1.setBounds(44, 31, 156, 16);
 		panel.add(lblNewLabel_1);
 
-		JButton btnNewButton = new JButton("Evaluate");
-		btnNewButton.setBounds(44, 245, 97, 25);
+		JButton btnNewButton = new JButton("caesar_encrypt");
+		btnNewButton.setBounds(187, 215, 128, 25);
 		panel.add(btnNewButton);
-//		txtr.setBounds(43, 104, 98, 98);
+		// txtr.setBounds(43, 104, 98, 98);
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(44, 104, 612, 124);
+		scrollPane.setBounds(44, 62, 612, 124);
 		panel.add(scrollPane);
 
 		JTextArea txtr = new JTextArea();
 		txtr.setRows(25);
 		txtr.setColumns(25);
-		
+
 		scrollPane.setViewportView(txtr);
 		// panel.add(txtr);
 		txtr.setLineWrap(true);
-		txtr.append("Pm ol ohk hufaopun jvumpkluaphs av zhf, ol dyval pa pu jpwoly, aoha pz, if zv johunpun aol vykly vm aol slaalyz vm aol hswohila, aoha uva h dvyk jvbsk il thkl vba.");
+		txtr.append("WELCOME TO THE ANDREJ FREQUENCY ANALYSIS PROJECT");
+		txtr.setRows(25);
+		txtr.setColumns(25);
+
+		JButton btnCaesardecrypt = new JButton("caesar_decrypt");
+		btnCaesardecrypt.setBounds(342, 215, 128, 25);
+		panel.add(btnCaesardecrypt);
+
+		JLabel label = new JLabel("plain_text / cipher_text");
+		label.setBounds(44, 271, 156, 16);
+		panel.add(label);
+		JPanel panelFA = new JPanel();
+		tabbedPane.addTab("Cracking Caesar-cipher with Frequency Analysis", null, panelFA, null);
+		panelFA.setLayout(null);
+
+		JLabel lblNewLabel_2 = new JLabel("Cipher Text");
+		lblNewLabel_2.setBounds(42, 24, 110, 16);
+		panelFA.add(lblNewLabel_2);
+
+		JButton btnNewButton1 = new JButton("Crack");
+		btnNewButton1.setBounds(301, 225, 97, 25);
+		panelFA.add(btnNewButton1);
+//		txtr.setBounds(43, 104, 98, 98);
+		JScrollPane scrollPane2 = new JScrollPane();
+		scrollPane2.setBounds(42, 53, 612, 98);
+		panelFA.add(scrollPane2);
+
+		JTextArea txtrFA = new JTextArea();
+
+		scrollPane2.setViewportView(txtrFA);
+		// panel.add(txtr);
+		txtrFA.setLineWrap(true);
+
+		JLabel lblNewLabel = new JLabel("Alphabet");
+		lblNewLabel.setBounds(42, 171, 63, 16);
+		panelFA.add(lblNewLabel);
+
+		txtAlphabet = new JTextField();
+		txtAlphabet.setText(" ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+		txtAlphabet.setEditable(false);
+		txtAlphabet.setBounds(117, 164, 537, 31);
+		panelFA.add(txtAlphabet);
+		txtAlphabet.setColumns(10);
+		JScrollPane scrollPane_1 = new JScrollPane();
+		scrollPane_1.setBounds(44, 303, 612, 124);
+		panel.add(scrollPane_1);
+		txtrFA.append("ZHOFRPHCWRCWKHCDQGUHMCIUHTXHQFACDQDOAVLVCSURMHFW");
 
 	}
 }
