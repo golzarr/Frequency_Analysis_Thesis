96d02b57ce7f04351a034255c557b5fb3919258f
unknown <andrej.zajac.dev@gmail.com>
2023-03-17 11:22:54 +0100

    Draft 2.0

--- target/classes/frequencyAnalysisSubstitutionCipherFA.py
+++ target/classes/frequencyAnalysisSubstitutionCipherFA.py
@@ -49,8 +49,48 @@ def crack(text):
     #for i in range(len(LETTERS)):
        #print(LETTERS[i])
        #out+=str(freq[i][0] +'-'+ str(freq[i][1])+"\n")
+    
     print(out)
     
+      
+    
+def printString(S, N):
+	
+	plaintext = [None] * 26
+	
+	freq = frequency_analysis(text)	
+	T = "ETAOINSHRDLCUMWFGYPBVKJXQZ"
+	curr = ""
+		
+	# Generate the probable ith plaintext
+	# string using the shift calculated above
+	for k in range(N):
+			x = ord(T[k]) - 65
+			if S[k] == ' ':
+				curr += " "
+				continue
+			
+			# Shift the kth letter of the
+			# cipher by x
+			y = ord(S[k]) - 65
+			y += x
+			
+			if y < 0:
+				y += 26
+			if y > 25:
+				y -= 26
+			
+			# Add the kth calculated/shifted
+			# letter to temporary string	
+			curr += chr(y + 65)
+		
+			plaintext[k] = curr
+	
+	for i in range(26):
+		print(plaintext[i])
+
+
+
     
     
 
@@ -73,3 +113,10 @@ if __name__ == '__main__':
 
 	#cipher_text = 'cey jeiia seii ke mle nfnie'
 	crack(text)
+	
+	S = text
+	N = len(S)
+
+	# Function Call
+	printString(S, N)
+    
